3fd97d8df50ee0125dfac3f3bb0f7689a037522b
aswartha <palla.aswartha@gmail.com>
2021-02-17 15:50:46 +0530

    First Commit

---
+++ src/main/java/com/boot/dockerjenkinspushtohub/DockerJenkinPushtoHubApplication.java
@@ -0,0 +1,13 @@
+package com.boot.dockerjenkinspushtohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinPushtoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinPushtoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/dockerjenkinspushtohub/DockerJenkinPushtoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.dockerjenkinspushtohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinPushtoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
